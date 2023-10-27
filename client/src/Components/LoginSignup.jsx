a52f08866a69a1ae0222a73066df3dda0be95ee1
Alosh Muhammad <alosh.km@gmail.com>
2023-10-27 08:59:46 +0530

    client side set up

---
+++ client/src/Components/LoginSignup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import email_icon from './Assets/email.png'
+import user_icon from './Assets/person.png'
+import password_icon from './Assets/password.png'
+
+const LoginSignup = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LoginSignup
